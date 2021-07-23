ce3b8d2aa1ada60f28348b0cb2886001430cef6e
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2021-07-23 11:16:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Todo list app
+> Simple app that manages a list of activities.
+
+# Screenshots
+![screenshot](./todoScreen.png)
+
+## Built With
+- html
+- sass
+- javaScript
+- Webpack
+
+## Live Demo
+You can see my page live here: 
+[Check it out!](https://gabrielyea.github.io/to-do-list/)
+
+
+## Getting Started
+To get a local copy running follow this steps!
+- Clone this repo.
+- `npm install` to install the packages
+- `npm run start` to build dist files and run local server.
+
+### Run tests
+To run the test just use the following command!
+- `npm test`
+
+<!--To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+### Setup
+
+### Install
+
+### Usage
+
+### Deployment-->
+
+
+## Authors
+
+👤 *Gabriel Rendon*
+
+- GitHub: [@gabrielyea](https://github.com/gabrielyea)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+<!--Feel free to check the [issues page](../../issues/).-->
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+<!--## Acknowledgments-->
+
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+/* eslint-disable global-require */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  mode: 'development',
+  entry: {
+    index: './src/scripts/index.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
